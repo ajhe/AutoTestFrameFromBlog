d2bbeb76278c00c82c75714e13f80a174efaf66a
ajhe <461123104@qq.com>
2018-09-19 17:50:44 +0800

    增加方法

--- pageobjects/baidu_homepage.py
+++ pageobjects/baidu_homepage.py
@@ -6,9 +6,14 @@ class HomePage(BasePage):
 
     input_box = "id=>kw"
     search_submit_btn = "xpath=>//*[@id='su']"
+    news_link = "xpath=>.//*[@id='u1']/a[1]"
 
     def type_input_box(self, text):
         self.type(self.input_box, text)
 
     def click_search_submit_btn(self):
         self.click(self.search_submit_btn)
+
+    def click_news(self):
+        self.click(self.news_link)
+        self.sleep(2)
---
+++ pageobjects/baidu_news_home.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+
+from framework.base_page import BasePage
+
+class NewsPage(BasePage):
+    pass
+
+    # sports_news_link =
